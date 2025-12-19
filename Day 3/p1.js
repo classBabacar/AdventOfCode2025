5c5e00e7cff72fcf6f64c793723a77fcb0f50d50
Babacar <bndoyecs@gmail.com>
2025-12-18 21:04:29 -0500

    Day 3 Solution

---
+++ Day 3/p1.js
@@ -0,0 +1,43 @@
+const { input } = require("./input");
+
+let maxJoltage = 0;
+
+input.forEach((line) => {
+  let bank = line;
+  let stackLimit = 2; // N digits max for the stack
+  let digitsUsable = 2; // can only choose N digits from a bank
+  let removalsAllowed = bank.length - digitsUsable; // how many times are we allowed to remove a digit, to hit our benchmark
+  let jolts = monotonicStack(bank, stackLimit, removalsAllowed);
+
+  const joltsAsNumber = jolts.reduce((accumulator, currentValue) => {
+    return accumulator * 10 + currentValue;
+  }, 0);
+
+  maxJoltage += joltsAsNumber;
+});
+
+function monotonicStack(bank, stackLimit, removalsAllowed) {
+  let stack = [];
+  for (let i = 0; i < bank.length; ++i) {
+    let battery = Number(bank[i]);
+
+    while (
+      !stack.length == 0 &&
+      removalsAllowed > 0 &&
+      battery > stack[stack.length - 1]
+    ) {
+      stack.pop();
+      removalsAllowed--;
+    }
+
+    stack.push(battery);
+    if (stack.length > stackLimit && removalsAllowed > 0) {
+      stack.pop();
+      removalsAllowed--;
+    }
+  }
+
+  return stack;
+}
+
+console.log("Max joltage across banks: ", maxJoltage);
